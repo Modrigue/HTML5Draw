6260a0522268c914d977bb6036357427d6ee5dfb
Modrigue <thealdobaldo@gmail.com>
2020-11-02 18:46:16 +0100

    updated download function

--- draw.js
+++ draw.js
@@ -146,8 +146,27 @@ if(window.addEventListener)
             var button_download = document.getElementById('buttondownload');
             button_download.addEventListener('click', function (e)
             {
-                var dataURL = canvas_img.toDataURL('image/png');
-                button_download.href = dataURL;
+                // obsolete
+                //var dataURL = canvas_img.toDataURL('image/png');
+                //button_download.href = dataURL;
+
+                let canvasImage = document.getElementById('imageView').toDataURL('image/png');
+
+                // this can be used to download any image from webpage to local disk
+                let xhr = new XMLHttpRequest();
+                xhr.responseType = 'blob';
+                xhr.onload = function () {
+                    let a = document.createElement('a');
+                    a.href = window.URL.createObjectURL(xhr.response);
+                    a.download = 'ImageWebDraw.png';
+                    a.style.display = 'none';
+                    document.body.appendChild(a);
+                    a.click();
+                    a.remove()
+                };
+
+                xhr.open('GET', canvasImage); // This is to download the canvas Image
+                xhr.send();
             });
         
             // activate default values
